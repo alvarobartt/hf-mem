294138ad99141e0f22c0836247fce5b1494458dc
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-03-10 16:44:36 +0100

    Add `clap` with `model-id` and `token` (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use clap::Parser;
 
 mod fetch;
 use fetch::fetch_metadata;
@@ -6,14 +7,35 @@ use fetch::fetch_metadata;
 mod token;
 use token::get_token;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    model_id: String,
+
+    #[arg(short, long)]
+    token: Option<String>,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    let url = "https://huggingface.co/gpt2/resolve/main/model.safetensors";
-    let token = get_token().context("the hugging face auth token couldn't be retrieved")?;
-    let metadata = fetch_metadata(url, &token)
+    let args = Args::parse();
+
+    let url = format!(
+        "https://huggingface.co/{}/resolve/main/model.safetensors",
+        args.model_id
+    );
+
+    let token = if let Some(token) = args.token {
+        token
+    } else {
+        get_token().context("the hugging face auth token couldn't be retrieved")?
+    };
+
+    let metadata = fetch_metadata(&url, &token)
         .await
         .context("failed to fetch metadata from the safetensors file from the hub")?;
-
     println!("metadata contains {metadata:?}");
+
     Ok(())
 }
