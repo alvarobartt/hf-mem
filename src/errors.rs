1484a9678e1f313cbb0ea48ffcf3091cc136351f
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-04-04 11:44:08 +0200

    Update `HubError` into `RequestError`

--- src/errors.rs
+++ src/errors.rs
@@ -1,15 +1,26 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum HubError {
-    #[error("file `{0}` not found on the hub")] // 404
+pub enum RequestError {
+    #[error("File `{0}` not found on the Hugging Face Hub.")]
     FileNotFound(String),
-    #[error("hub seems to be unavailable at the moment, retry later")] // 503
-    HubIsDown,
-    #[error("hub authentication failed, make sure that the hf token is valid")] // 403
+
+    #[error(
+        "Authentication to the Hugging Face Hub failed, please make sure you're using a valid Hugging Face Hub Token, more information at https://huggingface.co/docs/hub/security-tokens."
+    )]
     HubAuth,
-    #[error("internal error, make sure that the request is correct and retry later")] // 500
+
+    #[error(
+        "The Hugging Face Hub seems to be down at the moment, make sure to check https://status.huggingface.co for updates."
+    )]
+    HubIsDown,
+
+    #[error("Internal error, please make sure that the request is correct and/or retry later.")]
     Internal,
+
+    #[error("Request failed due to unknown reasons with status code `{0}`.")]
+    Unknown(reqwest::StatusCode),
+
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
