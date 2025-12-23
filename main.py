44c0e7818ce68f348073a1714466cdc9326a3cf4
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-12-23 13:38:53 +0100

    Add support for model_index.json for Diffusers (WIP)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import struct
 import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from functools import reduce
-from typing import Any, Dict
+from typing import Any, Dict, List
 from urllib.request import Request, urlopen
 
 # NOTE: Defines the bytes that will be fetched per safetensors file, but the metadata
@@ -108,8 +108,121 @@ def main(model_id: str, revision: str) -> None:
 
         metadata = reduce(lambda acc, metadata: acc | metadata, metadatas, {})
     elif "model_index.json" in files:
-        logging.warning("model_index.json NOT SUPPORTED YET")
-        sys.exit(1)
+        url = f"https://huggingface.co/{model_id}/resolve/{revision}/model_index.json"
+        request = Request(url, headers=headers, method="GET")
+        with urlopen(request, timeout=REQUEST_TIMEOUT) as response:
+            if response.status != 200:
+                logging.error(f"REQUEST FAILED WITH {response.status}")
+                sys.exit(3)
+
+            paths = {
+                k
+                for k, _ in json.loads(response.read()).items()
+                if not k.startswith("_")  # e.g. `_class_name` or `_diffusers_version`
+            }
+
+            path_urls: Dict[str, List[str]] = {}
+            for path in paths:
+                if path not in path_urls:
+                    path_urls[path] = []
+
+                if f"{path}/diffusion_pytorch_model.safetensors" in files:
+                    path_urls[path].append(
+                        f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/diffusion_pytorch_model.safetensors"
+                    )
+                elif f"{path}/model.safetensors" in files:
+                    path_urls[path].append(
+                        f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/model.safetensors"
+                    )
+                elif f"{path}/diffusion_pytorch_model.safetensors.index.json" in files:
+                    url = f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/diffusion_pytorch_model.safetensors.index.json"
+                    request = Request(url, headers=headers, method="GET")
+                    with urlopen(request, timeout=REQUEST_TIMEOUT) as response:
+                        if response.status != 200:
+                            logging.error(f"REQUEST FAILED WITH {response.status}")
+                            sys.exit(3)
+
+                        path_urls[path] = list({
+                            f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/{f}"
+                            for _, f in json.loads(response.read())[
+                                "weight_map"
+                            ].items()
+                        })
+                elif f"{path}/model.safetensors.index.json" in files:
+                    url = f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/model.safetensors.index.json"
+                    request = Request(url, headers=headers, method="GET")
+                    with urlopen(request, timeout=REQUEST_TIMEOUT) as response:
+                        if response.status != 200:
+                            logging.error(f"REQUEST FAILED WITH {response.status}")
+                            sys.exit(3)
+
+                        path_urls[path] = list({
+                            f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/{f}"
+                            for _, f in json.loads(response.read())[
+                                "weight_map"
+                            ].items()
+                        })
+
+        path_metadatas: Dict[str, List[Dict[str, Any]]] = {}
+        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+            futures = {
+                executor.submit(fetch_safetensors_metadata, url): path
+                for path, urls in path_urls.items()
+                for url in urls
+                if len(urls) > 0
+            }
+            for future in as_completed(futures):
+                path = futures[future]
+                try:
+                    if path not in path_metadatas:
+                        path_metadatas[path] = []
+                    path_metadatas[path].append(future.result())
+                except Exception as e:
+                    logging.error(f'ANY OF THE FUTURES FAILED "{e}"')
+                    sys.exit(4)
+
+        metadatas: Dict[str, Dict[str, Any]] = {
+            path: reduce(lambda acc, metadata: acc | metadata, metadatas, {})
+            for path, metadatas in path_metadatas.items()
+        }
+
+        for path, metadata in metadatas.items():
+            ppdt = {}
+            for key, value in metadata.items():
+                if key in {"__metadata__"}:
+                    continue
+                if value["dtype"] not in ppdt:
+                    ppdt[value["dtype"]] = 0
+                ppdt[value["dtype"]] += math.prod(value["shape"])
+
+            total = 0
+            for dtype, count in ppdt.items():
+                logging.info(f"[PATH={path}] DTYPE={dtype}, COUNT={count}\n")
+
+                match dtype:
+                    case "F64" | "I64" | "U64":
+                        dtype_b = 8
+                    case "F32" | "I32" | "U32":
+                        dtype_b = 4
+                    case "F16" | "BF16" | "I16" | "U16":
+                        dtype_b = 2
+                    case "F8_E5M2" | "F8_E4M3" | "I8" | "U8":
+                        dtype_b = 1
+                    case _:
+                        logging.error(f"DTYPE={dtype} NOT HANDLED")
+                        sys.exit(2)
+
+                total += count * dtype_b
+
+            logging.info(f"[PATH={path}] TOTAL VRAM={total} (IN BYTES)")
+            logging.info(
+                f"[PATH={path}] TOTAL VRAM={total / (1024**2):.2f} (IN MEGABYTES)"
+            )
+            logging.info(
+                f"[PATH={path}] TOTAL VRAM={total / (1024**3):.2f} (IN GIGABYTES)\n"
+            )
+
+        sys.exit(0)
     else:
         logging.error(
             "NONE OF `model.safetensors`, `model.safetensors.index.json`, `model_index.json` HAS BEEN FOUND"
