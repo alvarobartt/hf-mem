aac142119fc06028966726ba03f280b665a51814
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2026-01-07 03:45:21 +0900

    Add `--json-output` flag (#5)
    * Add `SafetensorsDtypes` and `get_safetensors_dtype_bytes`
    
    * Add `--json` (WIP)
    
    * Add `parse_safetensors_metadata` and `print_report` (refactor, WIP)
    
    * Fix `--json` output
    
    Run as `uv run hf-mem --model-id Qwen/Qwen-Image --json | jq`
    
    * Remove unused imports in `print.py`
    
    * Add `DtypeMetadata` and `ComponentMetadata` dataclasses
    
    * Use `dataclasses.asdict` to convert dataclass to dict
    
    * Some fixes here and there, before next release

--- src/hf_mem/cli.py
+++ src/hf_mem/cli.py
@@ -3,12 +3,14 @@ import asyncio
 import json
 import os
 import struct
+from dataclasses import asdict
 from functools import reduce
 from typing import Any, Dict, List, Optional
 
 import httpx
 
-from hf_mem.print import print_report_for_diffusers, print_report_for_transformers
+from hf_mem.metadata import parse_safetensors_metadata
+from hf_mem.print import print_report
 
 # NOTE: Defines the bytes that will be fetched per safetensors file, but the metadata
 # can indeed be larger than that
@@ -53,12 +55,17 @@ async def fetch_safetensors_metadata(
     return json.loads(metadata)
 
 
-async def run(model_id: str, revision: str, ignore_table_width: bool = False) -> None:
+async def run(
+    model_id: str,
+    revision: str,
+    json_output: bool = False,
+    ignore_table_width: bool = False,
+) -> Dict[str, Any] | None:
     headers = {}
     # NOTE: Read from `HF_TOKEN` if provided, then fallback to reading from `$HF_HOME/token`
-    if token := os.getenv("HF_TOKEN", None):
+    if token := os.getenv("HF_TOKEN"):
         headers["Authorization"] = f"Bearer {token}"
-    if "Authorization" not in headers:
+    elif "Authorization" not in headers:
         path = os.getenv("HF_HOME", ".cache/huggingface")
         filename = (
             os.path.join(os.path.expanduser("~"), path, "token")
@@ -83,26 +90,26 @@ async def run(model_id: str, revision: str, ignore_table_width: bool = False) ->
     # TODO: `recursive=true` shouldn't really be required unless it's a Diffusers
     # models... I don't think this adds extra latency anyway
     url = f"https://huggingface.co/api/models/{model_id}/tree/{revision}?recursive=true"
-
     files = await get_json_file(client=client, url=url, headers=headers)
-
-    file_paths = [
-        f["path"]
-        for f in files
-        if f.get("path", None) is not None and f.get("type", None) == "file"
-    ]
+    file_paths = [f["path"] for f in files if f.get("path") and f.get("type") == "file"]
 
     if "model.safetensors" in file_paths:
         url = f"https://huggingface.co/{model_id}/resolve/{revision}/model.safetensors"
-        metadata = await fetch_safetensors_metadata(
+        raw_metadata = await fetch_safetensors_metadata(
             client=client, url=url, headers=headers
         )
-        print_report_for_transformers(
-            model_id=model_id,
-            revision=revision,
-            metadata=metadata,
-            ignore_table_width=ignore_table_width,
-        )
+
+        # NOTE: Given that at the moment for Sentence Transformers only the Transformers module is considered, we
+        # set the default component name to `0_Transformer` as defined in modules.json
+        if "config_sentence_transformers.json" in file_paths:
+            raw_metadata = {"0_Transformer": raw_metadata}
+
+        # NOTE: If the model is a transformers model, then we simply set the component name to `Transformer`, to
+        # make sure that we provide the expected input to the `parse_safetensors_metadata`
+        if "__metadata__" in raw_metadata:
+            raw_metadata = {"Transformer": raw_metadata}
+
+        metadata = parse_safetensors_metadata(raw_metadata=raw_metadata)
     elif "model.safetensors.index.json" in file_paths:
         # TODO: We could eventually skip this request in favour of a greedy approach on trying to pull all the
         # files following the formatting `model-00000-of-00000.safetensors`
@@ -116,24 +123,30 @@ async def run(model_id: str, revision: str, ignore_table_width: bool = False) ->
 
         semaphore = asyncio.Semaphore(MAX_CONCURRENCY)
 
-        async def fetch_semaphore(url: str) -> Dict[str, Any]:
+        async def fetch_with_semaphore(url: str) -> Dict[str, Any]:
             async with semaphore:
                 return await fetch_safetensors_metadata(
                     client=client, url=url, headers=headers
                 )
 
-        tasks = [asyncio.create_task(fetch_semaphore(url)) for url in urls]
+        tasks = [asyncio.create_task(fetch_with_semaphore(url)) for url in urls]
         metadata_list: List[Dict[str, Any]] = await asyncio.gather(
             *tasks, return_exceptions=False
         )
 
-        metadata = reduce(lambda acc, metadata: acc | metadata, metadata_list, {})
-        print_report_for_transformers(
-            model_id=model_id,
-            revision=revision,
-            metadata=metadata,
-            ignore_table_width=ignore_table_width,
-        )
+        raw_metadata = reduce(lambda acc, metadata: acc | metadata, metadata_list, {})
+
+        # NOTE: Given that at the moment for Sentence Transformers only the Transformers module is considered, we
+        # set the default component name to `0_Transformer` as defined in modules.json
+        if "config_sentence_transformers.json" in file_paths:
+            raw_metadata = {"0_Transformer": raw_metadata}
+
+        # NOTE: If the model is a transformers model, then we simply set the component name to `Transformer`, to
+        # make sure that we provide the expected input to the `parse_safetensors_metadata`
+        if "__metadata__" in raw_metadata:
+            raw_metadata = {"Transformer": raw_metadata}
+
+        metadata = parse_safetensors_metadata(raw_metadata=raw_metadata)
     elif "model_index.json" in file_paths:
         url = f"https://huggingface.co/{model_id}/resolve/{revision}/model_index.json"
         files_index = await get_json_file(client=client, url=url, headers=headers)
@@ -154,47 +167,53 @@ async def run(model_id: str, revision: str, ignore_table_width: bool = False) ->
                 files_index = await get_json_file(
                     client=client, url=url, headers=headers
                 )
-                path_urls[path] = list({
+                path_urls[path] = [
                     f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/{f}"
-                    for _, f in files_index["weight_map"].items()  # type: ignore
-                })
+                    for f in set(files_index["weight_map"].values())
+                ]
             elif f"{path}/model.safetensors.index.json" in file_paths:
                 url = f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/model.safetensors.index.json"
                 files_index = await get_json_file(
                     client=client, url=url, headers=headers
                 )
-                path_urls[path] = list({
+                path_urls[path] = [
                     f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/{f}"
-                    for _, f in files_index["weight_map"].items()  # type: ignore
-                })
+                    for f in set(files_index["weight_map"].values())
+                ]
 
         semaphore = asyncio.Semaphore(MAX_CONCURRENCY)
 
-        async def fetch_semaphore(url: str) -> Dict[str, Any]:
+        async def fetch_with_semaphore(url: str) -> Dict[str, Any]:
             async with semaphore:
                 return await fetch_safetensors_metadata(
                     client=client, url=url, headers=headers
                 )
 
-        metadata = {}
+        raw_metadata = {}
         for path, urls in path_urls.items():
-            tasks = [asyncio.create_task(fetch_semaphore(url)) for url in urls]
+            tasks = [asyncio.create_task(fetch_with_semaphore(url)) for url in urls]
             metadata_list: List[Dict[str, Any]] = await asyncio.gather(
                 *tasks, return_exceptions=False
             )
-            metadata[path] = reduce(
+            raw_metadata[path] = reduce(
                 lambda acc, metadata: acc | metadata, metadata_list, {}
             )
 
-        print_report_for_diffusers(
+        metadata = parse_safetensors_metadata(raw_metadata=raw_metadata)
+    else:
+        raise RuntimeError(
+            "NONE OF `model.safetensors`, `model.safetensors.index.json`, `model_index.json` HAS BEEN FOUND"
+        )
+
+    if json_output:
+        out = {"model_id": model_id, "revision": revision, **asdict(metadata)}
+        print(json.dumps(out))
+    else:
+        print_report(
             model_id=model_id,
             revision=revision,
             metadata=metadata,
             ignore_table_width=ignore_table_width,
-        )
-    else:
-        raise RuntimeError(
-            "NONE OF `model.safetensors`, `model.safetensors.index.json`, `model_index.json` HAS BEEN FOUND"
         )
 
 
@@ -209,6 +228,11 @@ def main() -> None:
         help="Model revision on the Hugging Face Hub",
     )
     parser.add_argument(
+        "--json-output",
+        action="store_true",
+        help="Whether to provide the output as a JSON instead of printed as table.",
+    )
+    parser.add_argument(
         "--ignore-table-width",
         action="store_true",
         help="Whether to ignore the maximum recommended table width, in case the `--model-id` and/or `--revision` cause a row overflow when printing those.",
@@ -220,6 +244,8 @@ def main() -> None:
         run(
             model_id=args.model_id,
             revision=args.revision,
+            # NOTE: Below are the arguments that affect the output format
+            json_output=args.json_output,
             ignore_table_width=args.ignore_table_width,
         )
     )
