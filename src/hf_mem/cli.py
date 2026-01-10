cc73efc342e5f914f30f84118e6b7d21291bff4d
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2026-01-10 21:06:16 +0100

    Add missing `/{path}/` in URL

--- src/hf_mem/cli.py
+++ src/hf_mem/cli.py
@@ -70,7 +70,7 @@ async def fetch_modules_and_dense_metadata(
         # Safetensors weights and if so, it's a single `model.safetensors` file as the sharding has a default on
         # ~5Gb per file, and usually the extra `Dense` layers are not larger than that (usually not even close).
         dense_metadata[path] = await fetch_safetensors_metadata(
-            client=client, url=f"{url}/model.safetensors", headers=headers
+            client=client, url=f"{url}/{path}/model.safetensors", headers=headers
         )
 
     return dense_metadata
