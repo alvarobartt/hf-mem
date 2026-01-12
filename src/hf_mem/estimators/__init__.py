e529e1843ea4c57ccb62daae2f156a99057c7c6a
Sheikyon <sheikyon@proton.me>
2026-01-12 23:00:39 +0100

    Create estimators/ directory

---
+++ src/hf_mem/estimators/__init__.py
@@ -0,0 +1,8 @@
+"""
+estimators/
+==========
+This subpackage will group the necessary modules that will be recruited 
+for the estimation of memory consumption of each type/family of supported models.
+
+This includes: Transformers, Diffusers, Sentence Transformers, VLMs, MoE, Quantized Variants, etc.
+"""
