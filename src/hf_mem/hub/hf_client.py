77132d5b5304133207e3eb584b91781f57eb993e
Sheikyon <sheikyon@proton.me>
2026-01-12 18:01:44 +0100

    Implement core hub client logic

---
+++ src/hf_mem/hub/hf_client.py
@@ -0,0 +1,165 @@
+import asyncio
+from functools import reduce
+from typing import Any, Dict, List, Optional, Set
+
+import httpx
+
+from ..utils.metadata import parse_safetensors_metadata
+from ..print import print_report
+from .constants import MAX_CONCURRENCY
+from .safetensors_metadata import get_json_file, fetch_safetensors_metadata
+from ..estimators.sentence_transformers import fetch_modules_and_dense_metadata
+
+
+async def run(
+    model_id: str,
+    revision: str,
+    json_output: bool = False,
+    ignore_table_width: bool = False,
+) -> Dict[str, Any] | None:
+    headers = {}
+    # NOTE: Read from `HF_TOKEN` if provided, then fallback to reading from `$HF_HOME/token`
+    if token := os.getenv("HF_TOKEN"):
+        headers["Authorization"] = f"Bearer {token}"
+    elif "Authorization" not in headers:
+        path = os.getenv("HF_HOME", ".cache/huggingface")
+        filename = (
+            os.path.join(os.path.expanduser("~"), path, "token")
+            if not os.path.isabs(path)
+            else os.path.join(path, "token")
+        )
+        if os.path.exists(filename):
+            with open(filename, "r", encoding="utf-8") as f:
+                headers["Authorization"] = f"Bearer {f.read().strip()}"
+
+    client = httpx.AsyncClient(
+        limits=httpx.Limits(
+            max_keepalive_connections=MAX_CONCURRENCY,
+            max_connections=MAX_CONCURRENCY,
+        ),
+        timeout=httpx.Timeout(REQUEST_TIMEOUT),
+        http2=True,
+        follow_redirects=True,
+    )
+
+    # TODO: `recursive=true` shouldn't really be required unless it's a Diffusers
+    # models... I don't think this adds extra latency anyway
+    url = f"https://huggingface.co/api/models/{model_id}/tree/{revision}?recursive=true"
+    files = await get_json_file(client=client, url=url, headers=headers)
+    file_paths = [f["path"] for f in files if f.get("path") and f.get("type") == "file"]
+
+    if "model.safetensors" in file_paths:
+        url = f"https://huggingface.co/{model_id}/resolve/{revision}/model.safetensors"
+        raw_metadata = await fetch_safetensors_metadata(client=client, url=url, headers=headers)
+        if "config_sentence_transformers.json" in file_paths:
+            dense_metadata = (
+                {}
+                if "modules.json" not in file_paths
+                else await fetch_modules_and_dense_metadata(
+                    client=client, url=f"https://huggingface.co/{model_id}/resolve/{revision}", headers=headers
+                )
+            )
+            raw_metadata = {"0_Transformer": raw_metadata, **dense_metadata}
+        else:
+            # NOTE: If the model is a transformers model, then we simply set the component name to `Transformer`, to
+            # make sure that we provide the expected input to the `parse_safetensors_metadata`
+            raw_metadata = {"Transformer": raw_metadata}
+        metadata = parse_safetensors_metadata(raw_metadata=raw_metadata)
+
+    elif "model.safetensors.index.json" in file_paths:
+        # TODO: We could eventually skip this request in favour of a greedy approach on trying to pull all the
+        # files following the formatting `model-00000-of-00000.safetensors`
+        url = f"https://huggingface.co/{model_id}/resolve/{revision}/model.safetensors.index.json"
+        files_index = await get_json_file(client=client, url=url, headers=headers)
+        urls = {
+            f"https://huggingface.co/{model_id}/resolve/{revision}/{f}"
+            for f in set(files_index["weight_map"].values())
+        }
+        semaphore = asyncio.Semaphore(MAX_CONCURRENCY)
+
+        async def fetch_with_semaphore(url: str) -> Dict[str, Any]:
+            async with semaphore:
+                return await fetch_safetensors_metadata(client=client, url=url, headers=headers)
+
+        tasks = [asyncio.create_task(fetch_with_semaphore(url)) for url in urls]
+        metadata_list: List[Dict[str, Any]] = await asyncio.gather(*tasks, return_exceptions=False)
+        raw_metadata = reduce(lambda acc, metadata: acc | metadata, metadata_list, {})
+        if "config_sentence_transformers.json" in file_paths:
+            dense_metadata = (
+                {}
+                if "modules.json" not in file_paths
+                else await fetch_modules_and_dense_metadata(
+                    client=client, url=f"https://huggingface.co/{model_id}/resolve/{revision}", headers=headers
+                )
+            )
+            raw_metadata = {"0_Transformer": raw_metadata, **dense_metadata}
+        else:
+            # NOTE: If the model is a transformers model, then we simply set the component name to `Transformer`, to
+            # make sure that we provide the expected input to the `parse_safetensors_metadata`
+            raw_metadata = {"Transformer": raw_metadata}
+        metadata = parse_safetensors_metadata(raw_metadata=raw_metadata)
+
+    elif "model_index.json" in file_paths:
+        url = f"https://huggingface.co/{model_id}/resolve/{revision}/model_index.json"
+        files_index = await get_json_file(client=client, url=url, headers=headers)
+        paths = {k for k, _ in files_index.items() if not k.startswith("_")}
+        path_urls: Dict[str, List[str]] = {}
+        for path in paths:
+            if f"{path}/diffusion_pytorch_model.safetensors" in file_paths:
+                path_urls[path] = [
+                    f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/diffusion_pytorch_model.safetensors"
+                ]
+            elif f"{path}/model.safetensors" in file_paths:
+                path_urls[path] = [
+                    f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/model.safetensors"
+                ]
+            elif f"{path}/diffusion_pytorch_model.safetensors.index.json" in file_paths:
+                url = f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/diffusion_pytorch_model.safetensors.index.json"
+                files_index = await get_json_file(client=client, url=url, headers=headers)
+                path_urls[path] = [
+                    f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/{f}"
+                    for f in set(files_index["weight_map"].values())
+                ]
+            elif f"{path}/model.safetensors.index.json" in file_paths:
+                url = (
+                    f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/model.safetensors.index.json"
+                )
+                files_index = await get_json_file(client=client, url=url, headers=headers)
+                path_urls[path] = [
+                    f"https://huggingface.co/{model_id}/resolve/{revision}/{path}/{f}"
+                    for f in set(files_index["weight_map"].values())
+                ]
+        semaphore = asyncio.Semaphore(MAX_CONCURRENCY)
+
+        async def fetch_with_semaphore(url: str) -> Dict[str, Any]:
+            async with semaphore:
+                return await fetch_safetensors_metadata(client=client, url=url, headers=headers)
+
+        # NOTE: Given that we need to fetch the Safetensors metadata for multiple components on Diffusers models,
+        # to speed the download up and not block (await) the for-loop, we instead create all the tasks within a
+        # for-loop then we await for those outside
+        _tasks = {}
+        for path, urls in path_urls.items():
+            _tasks[path] = [asyncio.create_task(fetch_with_semaphore(url)) for url in urls]
+        await asyncio.gather(*[task for tasks in _tasks.values() for task in tasks], return_exceptions=False)
+        raw_metadata = {}
+        for path, tasks in _tasks.items():
+            metadata_list = [task.result() for task in tasks]
+            raw_metadata[path] = reduce(lambda acc, metadata: acc | metadata, metadata_list, {})
+        metadata = parse_safetensors_metadata(raw_metadata=raw_metadata)
+
+    else:
+        raise RuntimeError(
+            "NONE OF `model.safetensors`, `model.safetensors.index.json`, `model_index.json` HAS BEEN FOUND"
+        )
+
+    if json_output:
+        out = {"model_id": model_id, "revision": revision, **asdict(metadata)}
+        print(json.dumps(out))
+    else:
+        print_report(
+            model_id=model_id,
+            revision=revision,
+            metadata=metadata,
+            ignore_table_width=ignore_table_width,
+        )
