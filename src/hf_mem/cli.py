2a4ecb2f403d101198191b7e00b803c2f5c6b3d9
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2026-01-27 20:28:25 +0100

    Update User-Agent header with `hf-mem/0.4`

--- src/hf_mem/cli.py
+++ src/hf_mem/cli.py
@@ -90,7 +90,7 @@ async def run(
     json_output: bool = False,
     ignore_table_width: bool = False,
 ) -> Dict[str, Any] | None:
-    headers = {"User-Agent": f"hf-mem/0.3; id={uuid4()}; model_id={model_id}; revision={revision}"}
+    headers = {"User-Agent": f"hf-mem/0.4; id={uuid4()}; model_id={model_id}; revision={revision}"}
     # NOTE: Read from `HF_TOKEN` if provided, then fallback to reading from `$HF_HOME/token`
     if token := os.getenv("HF_TOKEN"):
         headers["Authorization"] = f"Bearer {token}"
