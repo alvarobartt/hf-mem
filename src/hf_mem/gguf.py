fc301a9d7ed1e4b6173f0ad9cf931a819b9cf426
diegovelilla <diegovelillarecio@gmail.com>
2026-02-01 20:01:59 +0100

    Added fetching and parsing of gguf metadata + weights size estimation.

--- src/hf_mem/gguf.py
+++ src/hf_mem/gguf.py
@@ -1,81 +1,283 @@
 from dataclasses import dataclass
-from typing import Literal, Dict, TypeGuard
-from hf_mem.metadata import DtypeMetadata
+from typing import Dict, Optional, Any, Callable
+from enum import IntEnum
+import httpx
+import struct
+import math
+from hf_mem.cli import REQUEST_TIMEOUT
 
-GGUFDtypes = Literal[
-    "F64", "I64",
-    "F32", "I32",
-    "F16", "BF16", "I16",
-    # .GGUF specific dtypes
-    "Q8_K", "Q6_K", "Q5_K", "Q4_K", "Q3_K", "Q2_K",
-    "IQ4_NL", "IQ4_XS", 
-    "IQ3_S", "IQ3_XXS", 
-    "IQ2_XXS", "IQ2_S", "IQ2_XS",
-    "IQ1_S", "IQ1_M",
-    "TQ1_0", "TQ2_0",
-    "MXFP4",
-    # .GGUF specific dtypes (legacy type, added for exhaustivity)
-    "Q8_0", "Q8_1", "Q5_0", "Q5_1", "Q4_0", "Q4_1",
-]
+SIZES = [100_000, 1_000_000, 10_000_000, 50_000_000, 100_000_000]
 
-GGUFDtypeBitsPerWeight: Dict[GGUFDtypes, float] = {
-    "F64": 64.0,
-    "I64": 64.0,
-    "F32": 32.0,
-    "I32": 32.0,
-    "F16": 16.0,
-    "BF16": 16.0,
-    "I16": 16.0,
+# --- GGUF Dtypes (weights) ---
+class GGUFDtype(IntEnum):
+    F32 = 0
+    F16 = 1
+    Q4_0 = 2
+    Q4_1 = 3
+    # Q4_2 = 4
+    # Q4_3 = 5
+    Q5_0 = 6
+    Q5_1 = 7
+    Q8_0 = 8
+    Q8_1 = 9
+    Q2_K = 10
+    Q3_K = 11
+    Q4_K = 12
+    Q5_K = 13
+    Q6_K = 14
+    Q8_K = 15
+    IQ2_XXS = 16
+    IQ2_XS = 17
+    IQ3_XXS = 18
+    IQ1_S = 19
+    IQ4_NL = 20
+    IQ3_S = 21
+    IQ2_S = 22
+    IQ4_XS = 23
+    I8 = 24
+    I16 = 25
+    I32 = 26
+    I64 = 27
+    F64 = 28
+    IQ1_M = 29
+    BF16 = 30
+    # Q4_0_4_4
+    # Q4_0_4_8
+    # Q4_0_8_8
+    TQ1_0 = 34
+    TQ2_0 = 35
+    # IQ4_NL_4_4 = 36
+    # IQ4_NL_4_8 = 37
+    # IQ4_NL_8_8 = 38
+    MXFP4 = 39
+    COUNT = 40 # For exhaustivity, not used
+
+# Conversion .gguf weight dtype -> bits-per-weight
+GGUFDtypeBitsPerWeight: Dict[GGUFDtype, float] = {
+    GGUFDtype.F64: 64.0,
+    GGUFDtype.I64: 64.0,
+    GGUFDtype.F32: 32.0,
+    GGUFDtype.I32: 32.0,
+    GGUFDtype.F16: 16.0,
+    GGUFDtype.BF16: 16.0,
+    GGUFDtype.I16: 16.0,
     # .GGUF specific dtypes
-    "Q8_K": 8.03125,
-    "Q6_K": 6.5625,
-    "Q5_K": 5.5,
-    "Q4_K": 4.5,
-    "Q3_K": 3.4375,
-    "Q2_K": 2.625,
-    "IQ4_NL": 4.5,
-    "IQ4_XS": 4.25,
-    "IQ3_S": 3.44,
-    "IQ3_XXS": 3.06,
-    "IQ2_XXS": 2.06,
-    "IQ2_S": 2.5,
-    "IQ2_XS": 2.31,
-    "IQ1_S": 1.56,
-    "IQ1_M": 1.75,
-    "TQ1_0": 1.6875,
-    "TQ2_0": 2.0625,
-    "MXFP4": 4.25,
+    GGUFDtype.Q8_K: 8.03125,
+    GGUFDtype.Q6_K: 6.5625,
+    GGUFDtype.Q5_K: 5.5,
+    GGUFDtype.Q4_K: 4.5,
+    GGUFDtype.Q3_K: 3.4375,
+    GGUFDtype.Q2_K: 2.625,
+    GGUFDtype.IQ4_NL: 4.5,
+    GGUFDtype.IQ4_XS: 4.25,
+    GGUFDtype.IQ3_S: 3.44,
+    GGUFDtype.IQ3_XXS: 3.06,
+    GGUFDtype.IQ2_XXS: 2.06,
+    GGUFDtype.IQ2_S: 2.5,
+    GGUFDtype.IQ2_XS: 2.31,
+    GGUFDtype.IQ1_S: 1.56,
+    GGUFDtype.IQ1_M: 1.75,
+    GGUFDtype.TQ1_0: 1.6875,
+    GGUFDtype.TQ2_0: 2.0625,
+    GGUFDtype.MXFP4: 4.25,
     # .GGUF specific dtypes (legacy type, added for exhaustivity)
-    "Q8_0": 8.5,
-    "Q8_1": 9.0,
-    "Q5_0": 5.5,
-    "Q5_1": 6.0,
-    "Q4_0": 4.5,
-    "Q4_1": 5.0,
+    GGUFDtype.Q8_0: 8.5,
+    GGUFDtype.Q8_1: 9.0,
+    GGUFDtype.Q5_0: 5.5,
+    GGUFDtype.Q5_1: 6.0,
+    GGUFDtype.Q4_0: 4.5,
+    GGUFDtype.Q4_1: 5.0,
 }
 
 
-# Very similar to ComponentMetadata, might want to generalize it / create father type of GGUFDtypes
+# --- Auxiliary read functions ---
+# Related to .gguf metadata dtypes, not weights dtypes
+def _read_string(raw_metadata: bytes, offset: int) -> tuple[str, int]:
+    length = struct.unpack("<Q", raw_metadata[offset:offset+8])[0]
+    offset += 8
+    key = raw_metadata[offset:offset+length].decode()
+    offset += length
+    return key, offset
+
+def _read_uint8(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<B", raw_metadata[offset:offset+1])[0]
+    offset += 1
+    return value, offset
+
+def _read_int8(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<b", raw_metadata[offset:offset+1])[0]
+    offset += 1
+    return value, offset
+
+def _read_uint16(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<H", raw_metadata[offset:offset+2])[0]
+    offset += 2
+    return value, offset
+
+def _read_int16(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<h", raw_metadata[offset:offset+2])[0]
+    offset += 2
+    return value, offset
+
+def _read_uint32(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<I", raw_metadata[offset:offset+4])[0]
+    offset += 4
+    return value, offset
+
+def _read_int32(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<i", raw_metadata[offset:offset+4])[0]
+    offset += 4
+    return value, offset
+
+def _read_float32(raw_metadata: bytes, offset: int) -> tuple[float, int]:
+    value = struct.unpack("<f", raw_metadata[offset:offset+4])[0]
+    offset += 4
+    return value, offset
+
+def _read_bool(raw_metadata: bytes, offset: int) -> tuple[bool, int]:
+    value = struct.unpack("<?", raw_metadata[offset:offset+1])[0]
+    offset += 1
+    return value, offset
+
+def _read_array(raw_metadata: bytes, offset: int) -> tuple[list[Any], int]:
+    value_type = struct.unpack("<I", raw_metadata[offset:offset+4])[0]
+    offset += 4
+    length = struct.unpack("<Q", raw_metadata[offset:offset+8])[0]
+    offset += 8
+    value = []
+    for i in range(length):
+        sub_value, offset = Parsers[value_type](raw_metadata, offset)
+        value.append(sub_value)
+    return value, offset
+
+def _read_uint64(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<Q", raw_metadata[offset:offset+8])[0]
+    offset += 8
+    return value, offset
+
+def _read_int64(raw_metadata: bytes, offset: int) -> tuple[int, int]:
+    value = struct.unpack("<q", raw_metadata[offset:offset+8])[0]
+    offset += 8
+    return value, offset
+
+def _read_float64(raw_metadata: bytes, offset: int) -> tuple[float, int]:
+    value = struct.unpack("<d", raw_metadata[offset:offset+8])[0]
+    offset += 8
+    return value, offset
+
+# --- GGUF Metadata Dtypes ---
+# Related to .gguf metadata dtypes, not weights dtypes
+class GGUFMetadataDtype(IntEnum):
+    UINT8 = 0
+    INT8 = 1
+    UINT16 = 2
+    INT16 = 3
+    UINT32 = 4
+    INT32 = 5
+    FLOAT32 = 6
+    BOOL = 7
+    STRING = 8
+    ARRAY = 9
+    UINT64 = 10
+    INT64 = 11
+    FLOAT64 = 12
+
+Parsers: Dict[GGUFMetadataDtype, Callable] = {
+    GGUFMetadataDtype.UINT8: _read_uint8,
+    GGUFMetadataDtype.INT8: _read_int8,
+    GGUFMetadataDtype.UINT16: _read_uint16,
+    GGUFMetadataDtype.INT16: _read_int16,
+    GGUFMetadataDtype.UINT32: _read_uint32,
+    GGUFMetadataDtype.INT32: _read_int32,
+    GGUFMetadataDtype.FLOAT32: _read_float32,
+    GGUFMetadataDtype.BOOL: _read_bool,
+    GGUFMetadataDtype.STRING: _read_string,
+    GGUFMetadataDtype.ARRAY: _read_array,
+    GGUFMetadataDtype.UINT64: _read_uint64,
+    GGUFMetadataDtype.INT64: _read_int64,
+    GGUFMetadataDtype.FLOAT64: _read_float64,
+}
+
+@dataclass
+class GGUFDtypeMetadata:
+    param_count: int
+    bytes_count: float
+
 @dataclass
 class GGUFComponentMetadata:
-    dtypes: Dict[GGUFDtypes, DtypeMetadata]
+    dtypes: Dict[GGUFDtype, GGUFDtypeMetadata]
     param_count: int
-    bytes_count: int
+    bytes_count: float
 
-# Very similar to SafetensorsMetadata
 @dataclass
 class GGUFMetadata:
     components: Dict[str, GGUFComponentMetadata]
     param_count: int
-    bytes_count: int
+    bytes_count: float
 
 
-def is_gguf_dtype(value: str) -> TypeGuard[GGUFDtypes]:
-    return value in GGUFDtypeBitsPerWeight
+def parse_gguf_metadata(raw_metadata: bytes) -> GGUFMetadata:
+    # Header
+    magic = raw_metadata[:4].decode("ascii")
+    version = struct.unpack("<I", raw_metadata[4:8])[0]
+    tensor_count = struct.unpack("<Q", raw_metadata[8:16])[0]
+    metadata_kv_count = struct.unpack("<Q", raw_metadata[16:24])[0]
+    offset = 24
 
-
-def get_gguf_dtype_bytes(dtype: str) -> float:
-    if not is_gguf_dtype(dtype):
-        raise RuntimeError(f"DTYPE={dtype} NOT HANDLED")
+    # KV Cache Metadata
+    for i in range(metadata_kv_count):
+        # Key
+        key, offset = _read_string(raw_metadata, offset)
+        value_type = version = struct.unpack("<I", raw_metadata[offset:offset+4])[0]
+        offset += 4
+        value, offset = Parsers[value_type](raw_metadata, offset)
     
-    return GGUFDtypeBitsPerWeight[dtype] / 8.0 # bit to byte conversion
+    # Tensor info
+    component = GGUFComponentMetadata(dtypes={}, param_count=0, bytes_count=0)
+    for i in range(tensor_count):
+        name, offset = _read_string(raw_metadata, offset)
+        n_dimensions, offset = _read_uint32(raw_metadata, offset)
+        dimensions = []
+        for _ in range(n_dimensions):
+            dim, offset = _read_uint64(raw_metadata, offset)
+            dimensions.append(dim)
+        tensor_type, offset = _read_uint32(raw_metadata, offset)
+        tensor_offset, offset = _read_uint64(raw_metadata, offset)
+
+        param_count = math.prod(dimensions)
+        bytes_count = GGUFDtypeBitsPerWeight[GGUFDtype(tensor_type)] / 8.0 * param_count
+        if GGUFDtype(tensor_type) in component.dtypes:
+            dtype = component.dtypes[GGUFDtype(tensor_type)]
+            dtype.param_count += param_count
+            dtype.bytes_count += bytes_count
+        else:
+            component.dtypes[GGUFDtype(tensor_type)] = GGUFDtypeMetadata(
+                param_count=param_count,
+                bytes_count=bytes_count,
+            )
+        component.param_count += param_count
+        component.bytes_count += bytes_count
+
+    return GGUFMetadata(
+        components={"Transformer": component},
+        param_count=component.param_count,
+        bytes_count=component.bytes_count,
+    )
+
+
+async def fetch_gguf_metadata(
+    client: httpx.AsyncClient, url: str, headers: Optional[Dict[str, str]] = None
+) -> GGUFMetadata:
+    for size in SIZES:
+        try: 
+            request_headers = {"Range": f"bytes=0-{size}", **(headers or {})}
+            response = await client.get(url, headers=request_headers, timeout=REQUEST_TIMEOUT)
+            response.raise_for_status()
+
+            raw_metadata = response.read()
+            processed_metadata = parse_gguf_metadata(raw_metadata)
+            return processed_metadata
+        except (struct.error, UnicodeDecodeError):
+            if size == SIZES[-1]:
+                raise RuntimeError(f"Failed to parse GGUF metadata from {url}, metadata larger than {size/1_000_000:.2f} MB.")
+            continue
